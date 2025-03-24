ffe0d6a8d58d75cec47d986eb39d4ce6d804c6e9
Swetha Sivasankararaj <110710815+Swetha5021@users.noreply.github.com>
2025-03-24 20:04:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-fetch('config.json')
-    .then(response => response.json())
-    .then(config => {
-        window.$crisp = [];
-        window.CRISP_WEBSITE_ID = config.CRISP_WEBSITE_ID;
-        
-        let script = document.createElement("script");
-        script.src = "https://client.crisp.chat/l.js";
-        script.async = 1;
-        document.head.appendChild(script);
-    })
-    .catch(error => console.error("Error loading Crisp configuration:", error));
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = "ab67dd3d-bebb-447b-8d02-fa96205fc26b";
+
+let script = document.createElement("script");
+script.src = "https://client.crisp.chat/l.js";
+script.async = 1;
+document.head.appendChild(script);
